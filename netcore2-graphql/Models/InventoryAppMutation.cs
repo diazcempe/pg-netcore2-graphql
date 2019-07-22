7880b8cc61dfadd2f425a6c0fdd60bf6e6538f3d
Nastio Diaz <nastio.diaz@lyteventures.com>
2019-07-22 12:48:59 +0800

    Initial Commit

---
+++ netcore2-graphql/Models/InventoryAppMutation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netcore2_graphql.Models
+{
+    public class InventoryAppMutation
+    {
+    }
+}
---
+++ netcore2-graphql/Models/InventoryAppSchema.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQL;
+using GraphQL.Types;
+
+namespace netcore2_graphql.Models
+{
+    public class InventoryAppSchema : Schema
+    {
+        public InventoryAppSchema(IDependencyResolver resolver) : base(resolver)
+        {
+            Query = resolver.Resolve<InventoryAppQuery>();
+            Mutation = resolver.Resolve<InventoryAppMutation>();
+        }
+    }
+}
