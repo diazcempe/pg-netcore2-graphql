3e40b8247f1dfa754093a93bd5bd0fce4f88eb33
Nastio Diaz <nastio.diaz@lyteventures.com>
2019-07-29 10:58:44 +0800

    Added GraphQL Controller, Query, Mutation

--- netcore2-graphql/Models/InventoryAppMutation.cs
+++ netcore2-graphql/Models/InventoryAppMutation.cs
@@ -2,10 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GraphQL.Types;
+using netcore2_graphql.Core.Data;
+using netcore2_graphql.Core.Models;
 
 namespace netcore2_graphql.Models
 {
-    public class InventoryAppMutation
+    public class InventoryAppMutation : ObjectGraphType
     {
+        public InventoryAppMutation(IProductRepository productRepo)
+        {
+            Name = "Mutation";
+
+            Field<ProductType>(
+                name: "createProduct", 
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<ProductInputType>> { Name = "product" }
+                ),
+                resolve: context =>
+                {
+                    var product = context.GetArgument<Product>("product");
+                    return productRepo.Add(product);
+                });
+        }
     }
 }
--- netcore2-graphql/Models/InventoryAppQuery.cs
+++ netcore2-graphql/Models/InventoryAppQuery.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GraphQL.Types;
+using netcore2_graphql.Core.Data;
 
 namespace netcore2_graphql.Models
 {
-    public class InventoryAppQuery
+    public class InventoryAppQuery : ObjectGraphType
     {
+        public InventoryAppQuery(IProductRepository productRepo)
+        {
+            Field<ProductType>(
+                name: "product",
+                arguments: new QueryArguments(new QueryArgument<IntGraphType> {Name = "id"}),
+                resolve: context => productRepo.Get(context.GetArgument<int>("id")));
+
+            Field<ListGraphType<ProductType>>(
+                name: "products",
+                resolve: context => productRepo.All());
+        }
     }
 }
