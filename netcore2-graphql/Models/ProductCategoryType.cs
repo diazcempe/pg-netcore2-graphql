2f5a92bb1613f842c58acc3311206608092e28e5
Nastio Diaz <nastio.diaz@lyteventures.com>
2019-07-29 10:55:34 +0800

    Added GraphQL types

---
+++ netcore2-graphql/Models/ProductCategoryType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQL.Types;
+using netcore2_graphql.Core.Models;
+using netcore2_graphql.Helpers;
+
+namespace netcore2_graphql.Models
+{
+    public class ProductCategoryType : ObjectGraphType<ProductCategory>
+    {
+        public ProductCategoryType(ContextServiceLocator contextServiceLocator)
+        {
+            Field(x => x.Id);
+            Field(x => x.Name, true);
+            Field<ListGraphType<ProductType>>("products",
+                arguments: new QueryArguments(new QueryArgument<IntGraphType> { Name = "id" }),
+                resolve: context => contextServiceLocator.ProductRepository.Get(context.Source.Id), description: "Products");
+        }
+    }
+}
---
+++ netcore2-graphql/Models/ProductType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQL.Types;
+using netcore2_graphql.Core.Models;
+
+namespace netcore2_graphql.Models
+{
+    public class ProductType : ObjectGraphType<Product>
+    {
+        public ProductType()
+        {
+            Field(x => x.Id);
+            Field(x => x.Name, true);
+            Field(x => x.Category.Name).Name("category");
+        }
+    }
+}
